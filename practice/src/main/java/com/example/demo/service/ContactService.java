8214a4b9d6024bec6931a07c226564c6fcb2d662
kou-saka <juntakasaka5240@gmail.com>
2025-05-09 11:41:54 +0900

    管理者向けにCRUD機能を実装

--- practice/src/main/java/com/example/demo/service/ContactService.java
+++ practice/src/main/java/com/example/demo/service/ContactService.java
@@ -1,6 +1,7 @@
 package com.example.demo.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import com.example.demo.entity.Contact;
 import com.example.demo.form.ContactForm;
@@ -17,4 +18,15 @@ public interface ContactService {
      * @return Contact エンティティのリスト
      */
     List<Contact> findAllContacts();   // これでDBから全件を返すと定義した
+    
+    /**
+     * ID で問い合わせ１件を取得する
+     */
+    Optional<Contact> findContactById(Long id);
+
+    /**
+     * 指定 ID の問い合わせを削除する
+     */
+    void deleteContact(Long id);
+
 }
